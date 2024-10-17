c8ecead060796d037b47ac48665a297d1ff0de36
Ameer Hasan <ameer.hasan.dev@gmail.com>
2024-10-16 21:16:54 -0400

    Update next.config.mjs to reflect new organization and project names

--- next.config.mjs
+++ next.config.mjs
@@ -13,8 +13,8 @@ export default withSentryConfig(nextConfig, {
   // For all available options, see:
   // https://github.com/getsentry/sentry-webpack-plugin#options
 
-  org: "javascript-mastery",
-  project: "care-pulse",
+  org: "Ameer Dev",
+  project: "Healthcare Management System",
 
   // Only print logs for uploading source maps in CI
   silent: !process.env.CI,
