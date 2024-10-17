f37e61dd854407432e4b4d79a3f37f4239987a5c
Ameer Hasan <ameer.hasan.dev@gmail.com>
2024-10-16 21:18:41 -0400

    update

--- next.config.mjs
+++ next.config.mjs
@@ -13,7 +13,7 @@ export default withSentryConfig(nextConfig, {
   // For all available options, see:
   // https://github.com/getsentry/sentry-webpack-plugin#options
 
-  org: "Ameer Dev",
+  org: "emsmc-0l",
   project: "Healthcare Management System",
 
   // Only print logs for uploading source maps in CI
